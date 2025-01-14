057cd9b9e2f335bf8fb355aade7292a99830fffa
Juan Casco <juancasco.neuquen@gmail.com>
2025-01-14 16:46:45 -0300

    2024-01-14 Proyecto con primeras views y urls V_01

--- Proyectoweb/urls.py
+++ Proyectoweb/urls.py
@@ -17,6 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from ProyectowebApp import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name="Home"),
+    path('servicios', views.servicios, name="Servicios"),
+    path('tienda', views.tienda, name="Tienda"),
+    path('blog', views.blog, name="Blog"),
+    path('contacto', views.contacto, name="Contacto"),
 ]
