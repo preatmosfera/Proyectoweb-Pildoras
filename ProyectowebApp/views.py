a6fa22d1710c5d305b63b8a54dfe07ee27a44c4e
Juan Casco <juancasco.neuquen@gmail.com>
2025-01-15 17:26:09 -0300

    2024-01-15 Se agregan a la App los html y se linquean desde la vista  V_03

--- ProyectowebApp/views.py
+++ ProyectowebApp/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Home")
+    return render(request, "ProyectowebApp/home.html")
 
 def servicios(request):
-    return HttpResponse("Servicios")
+    return render(request, "ProyectowebApp/servicios.html")
 
 def tienda(request):
-    return HttpResponse("Tienda")
+    return render(request, "ProyectowebApp/tienda.html")
 
 def blog(request):
-    return HttpResponse("Blog")
+    return render(request, "ProyectowebApp/blog.html")
 
 def contacto(request):
-    return HttpResponse("Contacto")
+    return render(request, "ProyectowebApp/contacto.html")
