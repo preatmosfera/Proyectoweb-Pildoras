3973b7ae64160c008a0eff8a5d4cdf4cd0869a3d
Juan Casco <juancasco.neuquen@gmail.com>
2025-01-20 16:57:22 -0300

    2024-01-20 Se hacen las gestiones para visualizar y almacenar la imagen de servicio V_09

--- ProyectowebApp/urls.py
+++ ProyectowebApp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
 from ProyectowebApp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name="Home"),
@@ -9,3 +11,5 @@ urlpatterns = [
     path('blog', views.blog, name="Blog"),
     path('contacto', views.contacto, name="Contacto"),
 ]
+
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
