8c657b3c628c036dd79d7dfa8403d1d0526b3ae3
Juan Casco <juancasco.neuquen@gmail.com>
2025-01-19 23:28:55 -0300

    2024-01-19 Se crea superusuario y se registra la app servicio en admin V_08

--- Proyectoweb/settings.py
+++ Proyectoweb/settings.py
@@ -105,7 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
+
+LANGUAGE_CODE = 'es-eu'
 
 TIME_ZONE = 'UTC'
 
