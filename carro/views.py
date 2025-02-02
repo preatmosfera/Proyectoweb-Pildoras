6edcfa4dfe9488eda5054d24a39edf8fdfeb944f
Juan Casco <juancasco.neuquen@gmail.com>
2025-02-01 21:51:02 -0300

    2024-02-1 Se crea la app carro y el archivo carro.py + vistas de carro V_24

--- carro/views.py
+++ carro/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .carro import Carro
+from tienda.models import Tienda
 
 # Create your views here.
+
+def agregar_producto(request, producto_id):
+    carro = Carro(request)
+    producto = Tienda.objects.get(id=producto_id)
+    carro.agregar(producto=producto)
+    return redirect("tienda")
+
+def eliminar_producto(request, producto_id):
+    carro = Carro(request)
+    producto = Tienda.objects.get(id=producto_id)
+    carro.eliminar(producto=producto)
+    return redirect("tienda")
+
+def restar_producto(request, producto_id):
+    carro = Carro(request)
+    producto = Tienda.objects.get(id=producto_id)
+    carro.restar_producto(producto=producto)
+    return redirect("tienda")
+
+def limpiar_carro(request):
+    carro = Carro(request)
+    carro.limpiar_carro()
+    return redirect("tienda")
