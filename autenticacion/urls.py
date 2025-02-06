e14c8110e27c9e0728bfed8ea3c545751ed99240
Juan Casco <juancasco.neuquen@gmail.com>
2025-02-06 16:54:39 -0300

    2024-02-06 Se crea el formulario de registro V_28

--- autenticacion/urls.py
+++ autenticacion/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from . import views
+from .views import VRegistro
 
 
 
 urlpatterns = [
     
-    path('', views.autenticacion, name="Autenticacion"),
+    path('', VRegistro.as_view(), name="Autenticacion"),
 ]
 
--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
-def autenticacion(request):
+class VRegistro(View):
 
+    def get(self, request):
+        form=UserCreationForm()
+        return render(request, "registro/registro.html", {"form":form})
 
-    return render(request, "registro/registro.html")
+    def post(self, request):
+        pass
