0d3a3469eed613adaa54556c852a5bf6ef70f393
Juan Casco <juancasco.neuquen@gmail.com>
2025-02-09 13:51:01 -0300

    2024-02-09 Se crea la vista de logear y template login y e link desde base a login V_30

--- autenticacion/urls.py
+++ autenticacion/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import VRegistro, cerrar_sesion
+from .views import VRegistro, cerrar_sesion, logear
 
 
 
@@ -8,5 +8,6 @@ urlpatterns = [
     
     path('', VRegistro.as_view(), name="Autenticacion"),
     path('cerrar_sesion', cerrar_sesion, name="cerrar_sesion"),
+    path('logear', logear, name="logear"),
 ]
 
